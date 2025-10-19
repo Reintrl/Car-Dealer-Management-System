0831f2d8011c5d008a0df1d46cf121d626a05c67
Danila Lameko <144149675+Reintrl@users.noreply.github.com>
2025-10-19 14:43:06 +0300

    add pages

---
+++ frontend/src/pages/DealerPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DealerList from '../components/DealerList';
+
+const DealerPage = () => {
+    return (
+        <div>
+            <DealerList />
+        </div>
+    );
+};
+
+export default DealerPage;
---
+++ frontend/src/pages/UserPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserList from '../components/UserList';
+
+const UserPage = () => {
+    return (
+        <div>
+            <UserList />
+        </div>
+    );
+};
+
+export default UserPage;
