09cc0e36235052bb68cdc0d19af20c5dddffc97a
Danila Lameko <144149675+Reintrl@users.noreply.github.com>
2025-10-19 14:42:44 +0300

    Create CarPage.jsx

---
+++ frontend/src/pages/CarPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarList from '../components/CarList';
+
+const CarsPage = () => {
+    return (
+        <div>
+            <CarList />
+        </div>
+    );
+};
+
+export default CarsPage;
